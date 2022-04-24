7dc201eda269f919781712ac320606b641c4848a
Leonel Godoy <60699907+aka-leonel@users.noreply.github.com>
2022-04-23 22:55:28 -0300

    Add project files.

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    internal class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } 
+        }
+        private static int accountNumberSeed = 1234;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public BankAccount(string name) 
+        {
+            this.Owner = name;            
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount("L.Godoy");
+            Console.WriteLine($"Account {myAccount.Number} was created for {myAccount.Owner} with {myAccount.Balance}");
+        }
+    }
+}
